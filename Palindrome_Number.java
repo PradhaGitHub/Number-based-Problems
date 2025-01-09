0cfe9dc0d5bafab7bac0ca0d60893fb8008b30b7
PradhaGitHub <dhivyapradhagtech12@gmail.com>
2025-01-09 23:21:19 +0530

    Update Palindrome_Number.java

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -1,3 +1,21 @@
+/*logic ==>
+Check if 121 is a palindrome:
+Original Number: 121
+Initial res = 0.
+Iteration 1:
+Extract last digit: 121 % 10 = 1.
+Update reversed: res = 0 * 10 + 1 = 1.
+Remove last digit: 121 / 10 = 12.
+Iteration 2:
+Extract last digit: 12 % 10 = 2.
+Update reversed: res = 1 * 10 + 2 = 12.
+Remove last digit: 12 / 10 = 1.
+Iteration 3:
+Extract last digit: 1 % 10 = 1.
+Update reversed: res = 12 * 10 + 1 = 121.
+Remove last digit: 1 / 10 = 0.
+Compare: 121 == 121 → Palindrome.
+*/
 import java.util.*;
 public class Palindrome_Number{
     public static void main(String args[]){
